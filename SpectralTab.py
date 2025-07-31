6ff4b702872e44043272bde0bbc510e458112e85
Paul Marganian <pmargani@nrao.edu>
2025-07-31 13:47:04 -0400

    abstracted shared behavior into OptionsTab

--- SpectralTab.py
+++ SpectralTab.py
@@ -11,9 +11,9 @@ import numpy as np
 from ScanData import ScanData
 from PlotData import PlotData
 from GfmTab import GfmTab
+from OptionsTab import OptionsTab
 
-
-class SpectralTab(GfmTab):
+class SpectralTab(OptionsTab):
 
     """
     A class for handling all VEGAS data in GFM.
@@ -28,21 +28,21 @@ class SpectralTab(GfmTab):
     ):
         super().__init__(parent, scanData, name, scanTypes)
 
-        self.options_panel = QWidget()
-        self.options_layout = QVBoxLayout(self.options_panel)
-        self.options_checkboxes = {}
-        self.options_panel.setLayout(self.options_layout)
-        plot_panel = QWidget()
-        plot_layout = QVBoxLayout(plot_panel)
-        plot_layout.addWidget(self.toolbar)
-        plot_layout.addWidget(self.canvas)
-        plot_panel.setLayout(plot_layout)
-        layout = QHBoxLayout(self)
-        vbox = QVBoxLayout()
-        vbox.addWidget(plot_panel)
-        layout.addLayout(vbox)
-        layout.addWidget(self.options_panel)
-        self.setLayout(layout)
+        # self.options_panel = QWidget()
+        # self.options_layout = QVBoxLayout(self.options_panel)
+        # self.options_checkboxes = {}
+        # self.options_panel.setLayout(self.options_layout)
+        # plot_panel = QWidget()
+        # plot_layout = QVBoxLayout(plot_panel)
+        # plot_layout.addWidget(self.toolbar)
+        # plot_layout.addWidget(self.canvas)
+        # plot_panel.setLayout(plot_layout)
+        # layout = QHBoxLayout(self)
+        # vbox = QVBoxLayout()
+        # vbox.addWidget(plot_panel)
+        # layout.addLayout(vbox)
+        # layout.addWidget(self.options_panel)
+        # self.setLayout(layout)
 
         # these are the options available for spectral data
         self.labels = ["beams", "pols", "phases", "IFs"]
@@ -55,41 +55,43 @@ class SpectralTab(GfmTab):
         self.xUnits = "Channels"
         self.xFreqRange = (1620, 1650)  # MHz
 
-    def display_scan_data(self, currentScanIndex):
-        "Called in response to a scan selection change.  Displays the default plot"
-        # save which scan index was selected
-        self.currentScanIndex = currentScanIndex
-        # get the scan data by index
-        scan = self.scanData.getScanDataByIndex(self.currentScanIndex)
-        # use the scan data to update the text edit and options panel
-        print(f"Displaying scan data for: {scan['scan']}, type: {type(scan)}")
-        # value = f"proj: {self.scanData.project}, scan: {scan['scan']}"  # removed text_edit
-        # Clear previous options
-        while self.options_layout.count():
-            item = self.options_layout.takeAt(0)
-            widget = item.widget()
-            if widget:
-                widget.setParent(None)
-        self.options_checkboxes.clear()
-        # Get scan options and create checkboxes
-        opts = self.scanData.getScanOptions(self.currentScanIndex, self.labels)
-        self.optionKeys = opts.keys()
-        for label, values in opts.items():
-            group_box = QGroupBox(label)
-            v_layout = QVBoxLayout()
-            checkboxes = []
-            for i, val in enumerate(values):
-                cb = QCheckBox(str(val))
-                # first checkbox is checked by default
-                if i == 0:
-                    cb.setChecked(True)
-                v_layout.addWidget(cb)
-                checkboxes.append(cb)
-                cb.stateChanged.connect(self.on_option_checkbox_changed)
-            group_box.setLayout(v_layout)
-            self.options_layout.addWidget(group_box)
-            self.options_checkboxes[label] = checkboxes
+    # def display_scan_data(self, currentScanIndex):
+        # super().display_scan_data(currentScanIndex)
+    #     "Called in response to a scan selection change.  Displays the default plot"
+    #     # save which scan index was selected
+    #     self.currentScanIndex = currentScanIndex
+    #     # get the scan data by index
+    #     scan = self.scanData.getScanDataByIndex(self.currentScanIndex)
+    #     # use the scan data to update the text edit and options panel
+    #     print(f"Displaying scan data for: {scan['scan']}, type: {type(scan)}")
+    #     # value = f"proj: {self.scanData.project}, scan: {scan['scan']}"  # removed text_edit
+    #     # Clear previous options
+    #     while self.options_layout.count():
+    #         item = self.options_layout.takeAt(0)
+    #         widget = item.widget()
+    #         if widget:
+    #             widget.setParent(None)
+    #     self.options_checkboxes.clear()
+    #     # Get scan options and create checkboxes
+    #     opts = self.scanData.getScanOptions(self.currentScanIndex, self.labels)
+    #     self.optionKeys = opts.keys()
+    #     for label, values in opts.items():
+    #         group_box = QGroupBox(label)
+    #         v_layout = QVBoxLayout()
+    #         checkboxes = []
+    #         for i, val in enumerate(values):
+    #             cb = QCheckBox(str(val))
+    #             # first checkbox is checked by default
+    #             if i == 0:
+    #                 cb.setChecked(True)
+    #             v_layout.addWidget(cb)
+    #             checkboxes.append(cb)
+    #             cb.stateChanged.connect(self.on_option_checkbox_changed)
+    #         group_box.setLayout(v_layout)
+    #         self.options_layout.addWidget(group_box)
+    #         self.options_checkboxes[label] = checkboxes
 
+    def add_additional_options(self):
         # Add a widget for selecting integration
         integration_layout = QHBoxLayout()
         integration_label = QLabel("integration")
@@ -130,36 +132,36 @@ class SpectralTab(GfmTab):
         # self.options_layout.addWidget(self.integration_text_edit)
         #
         # Trigger the checkbox change handler to update the plot
-        self.on_option_checkbox_changed()
+        # self.on_option_checkbox_changed()
 
-    def on_option_checkbox_changed(self):
-        "Called when an option checkbox is changed.  Updates the plot based on selected options."
-        # find the selected values from the checkboxes
-        # labels = ["beams", "pols", "phases", "freqs"]
-        labels = self.optionKeys
-        selected_values = {}
-        for label in labels:
-            selected_values[label] = []
-            checkboxes = self.options_checkboxes.get(label, [])
-            for cb in checkboxes:
-                if cb.isChecked():
-                    val = cb.text()
-                    try:
-                        val = int(val)
-                    except ValueError:
-                        try:
-                            val = float(val)
-                        except ValueError:
-                            pass
-                    selected_values[label].append(val)
-        # if any option has NO selected values, do not plot
-        if any(len(selected_values[label]) == 0 for label in labels):
-            return
-        # turn this dict into a list of all combinations of selected values
-        value_lists = [selected_values[label] for label in labels]
-        key_combinations = list(itertools.product(*value_lists))
-        print(f"key_combinations: {key_combinations}")
-        self.plot_data(self.currentScanIndex, key_combinations)
+    # def on_option_checkbox_changed(self):
+    #     "Called when an option checkbox is changed.  Updates the plot based on selected options."
+    #     # find the selected values from the checkboxes
+    #     # labels = ["beams", "pols", "phases", "freqs"]
+    #     labels = self.optionKeys
+    #     selected_values = {}
+    #     for label in labels:
+    #         selected_values[label] = []
+    #         checkboxes = self.options_checkboxes.get(label, [])
+    #         for cb in checkboxes:
+    #             if cb.isChecked():
+    #                 val = cb.text()
+    #                 try:
+    #                     val = int(val)
+    #                 except ValueError:
+    #                     try:
+    #                         val = float(val)
+    #                     except ValueError:
+    #                         pass
+    #                 selected_values[label].append(val)
+    #     # if any option has NO selected values, do not plot
+    #     if any(len(selected_values[label]) == 0 for label in labels):
+    #         return
+    #     # turn this dict into a list of all combinations of selected values
+    #     value_lists = [selected_values[label] for label in labels]
+    #     key_combinations = list(itertools.product(*value_lists))
+    #     print(f"key_combinations: {key_combinations}")
+    #     self.plot_data(self.currentScanIndex, key_combinations)
 
     def plot_data(self, scanIndex : int, optionsKeys : list):
         "Uses ScanData and PlotData to create a matplotlib figure and refreshes the canvas"
